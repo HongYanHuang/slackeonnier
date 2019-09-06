0766c65eb7400c703339761923b9fec0bdcbc823
henry48124@gmail.com <henry48124@gmail.com>
2019-09-06 17:50:37 +0800

    stop when param failed

--- R/attachment_format.R
+++ R/attachment_format.R
@@ -12,11 +12,14 @@ attachment_format <- function(former_text,
                               status_color,
                               image_url = NULL,
                               image_title = " ") {
+  if(length(former_text) > 1) {stop("text must be a vector of length one")}
+  if(is.na(strtoi(gsub("#", "", status_color), 16))) {stop("not a hex code color")}
   if(is.null(image_url)) {
     attachment <- jsonlite::toJSON(
       data.frame(text = former_text,
                  color = status_color,
                  mrkdwn_in = "text"))
+    warning("No image url provided.")
   } else {
     attachment <- jsonlite::toJSON(
       data.frame(fallback = "ABC",

32cb5500e001b14bb8e33f871291c141a46a0090
henry48124@gmail.com <henry48124@gmail.com>
2019-09-06 18:08:07 +0800

    stop when param failed

--- R/slackeonnier_sent.R
+++ R/slackeonnier_sent.R
@@ -12,6 +12,7 @@ slackeonnier_sent <- function(attachment,
                           webhook = Sys.getenv("SLACK_INCOMING_WEBHOOK_URL"),
                           channel = Sys.getenv("SLACK_SENT_CHANNEL"),
                           username = Sys.getenv("SLACK_USERNAME")) {
+  if(!jsonlite::validate(attachment)[1]) {stop("Not a .json format, please format using attachment_format()")}
   resp <- httr::POST(url = webhook,
                encode = "form",
                add_headers(`Content-Type` = "application/x-www-form-urlencoded",
